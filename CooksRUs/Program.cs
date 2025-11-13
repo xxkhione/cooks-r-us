835ac48a01e0c5365a43d3aa1e7d54d12a4263f7
Dami <68552016+xxkhione@users.noreply.github.com>
2025-11-12 18:14:44 -0700

    Project Set-up

---
+++ CooksRUs/Program.cs
@@ -0,0 +1,32 @@
+using CooksRUs.Components;
+
+namespace CooksRUs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
